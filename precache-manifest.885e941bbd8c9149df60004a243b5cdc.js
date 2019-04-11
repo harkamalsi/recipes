bb0783d3036e949e7d658890589e00925def1394
harkamalsi <harkamal_guraya@hotmail.com>
2019-04-11 23:17:03 +0200

    Updates

---
+++ precache-manifest.885e941bbd8c9149df60004a243b5cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "8eae6469f295d3c14c28",
+    "url": "/recipes/static/js/main.79df54f5.chunk.js"
+  },
+  {
+    "revision": "47fe333da47bb5d0ba4e",
+    "url": "/recipes/static/js/2.3e9e2c5d.chunk.js"
+  },
+  {
+    "revision": "8eae6469f295d3c14c28",
+    "url": "/recipes/static/css/main.03176e78.chunk.css"
+  },
+  {
+    "revision": "47fe333da47bb5d0ba4e",
+    "url": "/recipes/static/css/2.26f1a30c.chunk.css"
+  },
+  {
+    "revision": "de17bc03db75a7c0e85fc11ebc8c6254",
+    "url": "/recipes/index.html"
+  }
+];
