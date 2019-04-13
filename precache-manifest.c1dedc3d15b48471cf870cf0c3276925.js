7e7a908cfd2e495547c4921d622d3b036a7f90dc
harkamalsi <harkamal_guraya@hotmail.com>
2019-04-13 10:32:01 +0200

    Updates

---
+++ precache-manifest.c1dedc3d15b48471cf870cf0c3276925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "e40b811e6a11f4c25c3c",
+    "url": "/recipes/static/js/main.3edcec41.chunk.js"
+  },
+  {
+    "revision": "8f42b616909f50ba733b",
+    "url": "/recipes/static/js/2.1620fa77.chunk.js"
+  },
+  {
+    "revision": "e40b811e6a11f4c25c3c",
+    "url": "/recipes/static/css/main.b2d8febf.chunk.css"
+  },
+  {
+    "revision": "8f42b616909f50ba733b",
+    "url": "/recipes/static/css/2.26f1a30c.chunk.css"
+  },
+  {
+    "revision": "76ed0842d01db9489d411a664961a305",
+    "url": "/recipes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/recipes/precache-manifest.6ed2e084c1ec94a737b11d4e742f2d68.js"
+  "/recipes/precache-manifest.c1dedc3d15b48471cf870cf0c3276925.js"
 );
 
 workbox.clientsClaim();
