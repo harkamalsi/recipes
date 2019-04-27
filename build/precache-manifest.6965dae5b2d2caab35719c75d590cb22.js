fd98c4df662db055863b01ee1f11860d37fc047c
harkamalsi <harkamal_guraya@hotmail.com>
2019-04-27 18:44:10 +0200

    last files

---
+++ build/precache-manifest.6965dae5b2d2caab35719c75d590cb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "48b8891bef9b937e560c",
+    "url": "/recipes/static/js/main.d3034e95.chunk.js"
+  },
+  {
+    "revision": "9f40b8ffc809ac223fcf",
+    "url": "/recipes/static/js/2.88bb8bc2.chunk.js"
+  },
+  {
+    "revision": "48b8891bef9b937e560c",
+    "url": "/recipes/static/css/main.cd540e80.chunk.css"
+  },
+  {
+    "revision": "9f40b8ffc809ac223fcf",
+    "url": "/recipes/static/css/2.26f1a30c.chunk.css"
+  },
+  {
+    "revision": "90ea4a2bce0cde6740332010e0adefa9",
+    "url": "/recipes/index.html"
+  }
+];
